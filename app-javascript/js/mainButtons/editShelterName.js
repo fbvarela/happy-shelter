4d49a3c24ef581aada08ee04ad9ce3569cbcca34
Fernando Varela <fernando@MacBook-Pro-de-Fernando.local>
2020-04-18 13:59:11 +0200

    remove javascript project

--- app-javascript/js/mainButtons/editShelterName.js
+++
@@ -1,8 +0,0 @@
-$('#editShelter').click(function() {
-  var defaultName = $('#shelterName').text();
-  var shelterName = prompt(getTextLang('shelterName').text, defaultName);
-  if(shelterName==null || shelterName.length==0){
-    return false;
-  }
-  $('#shelterName').text(shelterName);
-});
